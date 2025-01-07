59f74572cb0d3c6cf61588f6daa67dbac29f34c1
fengjun <134389159+JupiterFeng@users.noreply.github.com>
2025-01-07 13:05:00 +0800

    仓鼠

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 
 # 注册插件
-@register(name="LqunLanguage", description="萝群语录", version="1.0.1", author="jianrenjun")
+@register(name="LqunLanguage", description="萝群语录", version="1.0.2", author="jianrenjun")
 class MyPlugin(BasePlugin):
 
     # 插件加载时触发
